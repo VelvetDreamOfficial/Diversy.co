3f3ce3ee77b469b0e6e8b099da2f5294bc857b5a
nekoniyah <nekomancers0@gmail.com>
2025-05-11 16:10:28 +0200

    port variable

--- server/index.ts
+++ server/index.ts
@@ -13,4 +13,6 @@ const static_files = express.static("out");
 app.use("/", static_files);
 app.use("/api", router);
 
-server.listen(3000, () => console.log("Listening on port 3000"));
+server.listen(process.env.PORT || 3000, () =>
+    console.log("Listening on port 3000")
+);
